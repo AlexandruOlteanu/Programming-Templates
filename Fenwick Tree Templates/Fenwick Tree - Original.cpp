beca9e23f133c9e1891d84056930fb4a8becb298
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-04 18:24:30 +0300

    Update Fenwick Tree - Original.cpp

--- Fenwick Tree Templates/Fenwick Tree - Original.cpp
+++ Fenwick Tree Templates/Fenwick Tree - Original.cpp
@@ -1,19 +1,27 @@
 /*
     Template created by Alexandru Olteanu
 */
-ll fenw[maxn];
-void modify_fenwick(int x,int n, ll val){
-    for(; x <= n; x += x & -x){
-        fenw[x] += val;
+template<typename A>
+struct fenwick{
+    vector<A> fenw;
+    int n;
+    fenwick(int length){
+        fenw.resize(length);
+        n = length - 1;
     }
-    return;
-}
 
-ll get_fenwick(int x){
-    ll val = 0;
-    for(; x > 0; x -= x & -x){
-        val += fenw[x];
+    void modify_fenwick(int x, ll val){
+        for(; x <= n; x += x & -x){
+            fenw[x] += val;
+        }
+        return;
     }
-    return val;
-}
 
+    ll get_fenwick(int x){
+        ll val = 0;
+        for(; x > 0; x -= x & -x){
+            val += fenw[x];
+        }
+        return val;
+    }
+};
