a227da26a9ba978d5ff88e258e00283e936f8f4d
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-06 16:42:47 +0300

    Update main.cpp

--- Main Template - Alexandru Olteanu/main.cpp
+++ Main Template - Alexandru Olteanu/main.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 // GCC Optimizations
 #pragma GCC optimize("Ofast")
-#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt")
+#pragma GCC target("abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize(3)
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
 #pragma GCC optimize("unroll-loops")
 // Useful
 mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
