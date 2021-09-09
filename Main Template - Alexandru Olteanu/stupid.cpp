76908f7634b9f2a778e3311107dcc0aff5a2b37a
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-09 10:18:13 +0300

    Update stupid.cpp

--- Main Template - Alexandru Olteanu/stupid.cpp
+++ Main Template - Alexandru Olteanu/stupid.cpp
@@ -28,8 +28,8 @@ typedef pair<ll,ll> pll;
 ll const mod=1000000007LL;
 ll const mod2 = 100000000LL;
 ll const md=998244353LL;
-ll mypowr(ll a,ll b) {ll res=1;if(b<0)b=0;a%=mod; assert(b>=0);
-for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll mypowr(ll a,ll b, ll mod1) {ll res=1;if(b<0)b=0;a%=mod1; assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a%mod1;a=a*a%mod1;}return res;}
 ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
 for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
 ifstream in("input.txt");
@@ -40,12 +40,12 @@ ofstream out("output.txt");
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
 
-// #define cin in
-// #define cout out
+ //#define cin in
+ //#define cout out
 
 const ll infll = 9e18;
 const int inf = 2e9;
-const ll maxn = 3e5 + 2;
+const ll maxn = 2e5 + 2;
 
 
 
@@ -54,10 +54,11 @@ int main()
     FastEverything
     HighPrecision
     int test = 1;
-    //cin>>test;
+    cin>>test;
     for(int tt = 1; tt <= test; ++tt){
         
         
-    }
+
+    }   
     return 0;
 }
