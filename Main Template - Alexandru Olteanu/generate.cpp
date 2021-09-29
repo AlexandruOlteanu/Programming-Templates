e94f83a02f7bb397473361c5865cadb3d1d03306
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-29 14:48:13 +0300

    Update generate.cpp

--- Main Template - Alexandru Olteanu/generate.cpp
+++ Main Template - Alexandru Olteanu/generate.cpp
@@ -1,30 +1,29 @@
 /*
-    Programmer : Alexandru_Olteanu
+    Programmer : Alexandru Olteanu
 */
 #include<bits/stdc++.h>
 using namespace std;
 // GCC Optimizations
-#pragma GCC optimize("Ofast")
-#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt")
-#pragma GCC target("abm,mmx,avx,avx2,tune=native")
-#pragma GCC optimize(3)
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
+// #pragma GCC optimize("Ofast");
+// #pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt")
+// #pragma GCC target("abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize(3)
+// #pragma GCC optimize("inline")
+// #pragma GCC optimize("-fgcse")
+// #pragma GCC optimize("-fgcse-lm")
+// #pragma GCC optimize("-fipa-sra")
+// #pragma GCC optimize("-ftree-pre")
+// #pragma GCC optimize("-ftree-vrp")
+// #pragma GCC optimize("-fpeephole2")
+// #pragma GCC optimize("-ffast-math")
+// #pragma GCC optimize("-fsched-spec")
+// #pragma GCC optimize("unroll-loops")
 // Useful
 mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
-#define FastEverything  ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define FastEverything  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define HighPrecision cout<<fixed<<setprecision(17);
 typedef long long ll;
 typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
 ll const mod=1000000007LL;
 ll const mod2 = 100000000LL;
 ll const md=998244353LL;
@@ -32,20 +31,20 @@ ll mypowr(ll a,ll b, ll mod1) {ll res=1;if(b<0)b=0;a%=mod1; assert(b>=0);
 for(;b;b>>=1){if(b&1)res=res*a%mod1;a=a*a%mod1;}return res;}
 ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
 for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
-ifstream in("input.txt");
-ofstream out("output.txt");
 #define pb push_back
 #define fi first
 #define se second
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
 
- //#define cin in
- //#define cout out
+// ifstream in("input.txt");
+// ofstream out("output.txt");
+// #define cin in
+// #define cout out
 
 const ll infll = 9e18;
 const int inf = 2e9;
-const ll maxn = 2e5 + 2;
+const ll maxn = 1e5 + 2;
 
 
 
@@ -54,11 +53,12 @@ int main()
     FastEverything
     HighPrecision
     int test = 1;
-    cin>>test;
+    //cin>>test;
     for(int tt = 1; tt <= test; ++tt){
         
-        
 
-    }   
+    }       
+
+
     return 0;
-}
+} 
