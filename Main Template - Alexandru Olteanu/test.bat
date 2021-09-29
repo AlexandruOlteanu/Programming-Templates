79a1083a053905211645be6883f846c4098881e5
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-29 14:50:29 +0300

    Update test.bat

--- Main Template - Alexandru Olteanu/test.bat
+++ Main Template - Alexandru Olteanu/test.bat
@@ -1,7 +1,7 @@
 @echo off
 generate >input.txt
 main <input.txt >output.txt
-stupid <input.txt >stupid_output.txt
-fc output.txt stupid_output.txt
+second <input.txt >second_output.txt
+fc output.txt second_output.txt
 if errorlevel 1 exit
 test
