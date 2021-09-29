0796016b5a5b0a85cd7be2796a3812812f76ffce
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-29 14:33:32 +0300

    Update SegmentTree - Multiple Functions.cpp

--- Segment Tree/SegmentTree - Multiple Functions.cpp
+++ Segment Tree/SegmentTree - Multiple Functions.cpp
@@ -1,3 +1,6 @@
+/*
+    Template created by Alexandru Olteanu
+*/
 template<typename A>
 struct SegmentTree{
     vector<A> array;
@@ -5,70 +8,70 @@ struct SegmentTree{
     vector<A> lazy;
 
     SegmentTree(int n){
-        array.resize(n, 0);
-        tree.resize(4 * n + 1, 0);
-        lazy.resize(4 * n + 1, 0);
+        array.resize(n);
+        tree.resize(4 * n + 1);
+        lazy.resize(4 * n + 1);
     }
 
-    A function(A a, A b, int p){
-        if(p == 1) return (a > b ? a : b); //Probably it needs changes
-        return (a < b ? a : b);
+    A func(A a, A b, int p){
+        if(p == 1) return min(a, b);
+        return max(a, b);       //Probably it needs changes
     }
 
-    void build_segment(int node, int l, int r, int p){
+    void build(int node, int l, int r, int p){
         if(l == r){
             tree[node] = array[l];
             return;
         }
         int mid = l + (r - l) / 2;
-        build_segment(node * 2, l, mid, p);
-        build_segment(node * 2 + 1, mid + 1, r, p);
-        tree[node] = function(tree[node * 2], tree[node * 2 + 1], p);
+        build(node * 2, l, mid, p);
+        build(node * 2 + 1, mid + 1, r, p);
+        tree[node] = func(tree[node * 2], tree[node * 2 + 1], p);
         return;
     }
 
-    void push_segment(int node, int l, int r, int p){
+    void push(int node, int l, int r, p){
         if(lazy[node] != 0){
             if(l != r){
-                tree[node] = function(tree[node * 2], tree[node * 2 + 1], p); //Probably it needs changes
-                lazy[node * 2] ^= 1;
+                tree[node] = func(tree[node * 2], tree[node * 2 + 1], p);
+                lazy[node * 2] ^= 1;                    //Probably it needs changes
                 lazy[node * 2 + 1] ^= 1;
             }
             else{
-                tree[node] = array[l];
+                tree[node] = array[l];   
             }
             lazy[node] = 0;
         }
         return;
     }
 
-    void update_segment(int node, int l, int r, int L, int R, int p){
-        push_segment(node, l, r, p);
+    void update(int node, int l, int r, int L, int R, p){
+        push(node, l, r, p);
         if(r < L || l > R)return;
         if(l >= L && r <= R){
             lazy[node] ^= 1;
-            push_segment(node, l, r, p);
+            push(node, l, r);
             return;
         }
         int mid = l + (r - l) / 2;
-        update_segment(node * 2, l, mid, L, R, p);
-        update_segment(node * 2 + 1, mid + 1, r, L, R, p);
-        tree[node] = function(tree[node * 2], tree[node * 2 + 1], p);
+        update(node * 2, l, mid, L, R, p);
+        update(node * 2 + 1, mid + 1, r, L, R, p);
+        tree[node] = func(tree[node * 2], tree[node * 2 + 1], p);
         return;
     }
 
-    A ask_segment(int node, int l, int r, int L, int R, int p){
-        push_segment(node, l, r, p);
+    A get(int node, int l, int r, int L, int R, int p){
+        push(node, l, r, p);
         if(l >= L && r <= R){
             return tree[node];
         }
         int mid = l + (r - l) / 2;
         if(mid < L){
-            return ask_segment(node * 2 + 1, mid + 1, r, L, R, p);
+            return get(node * 2 + 1, mid + 1, r, L, R, p);
         }
         if(mid >= R){
-            return ask_segment(node * 2, l, mid, L, R, p);
+            return get(node * 2, l, mid, L, R, p);
         }
-        return function(ask_segment(node * 2, l, mid, L, R, p), ask_segment(node * 2 + 1, mid + 1, r, L, R, p), p);
+        return func(get(node * 2, l, mid, L, R, p), get(node * 2 + 1, mid + 1, r, L, R, p), p);
     }
 };
