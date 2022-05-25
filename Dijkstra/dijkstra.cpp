c44f3c8e3a5e8a8c00f76fdcdd584d7cce12dde3
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-05-25 19:40:19 +0300

    Update dijkstra.cpp

--- Dijkstra/dijkstra.cpp
+++ Dijkstra/dijkstra.cpp
@@ -1,17 +1,28 @@
-vector<pair<ll, int>> v[1002];
-vector<ll> dist(1002, infll);
-priority_queue<pll> q;
+const int inf = 2e9;
+const ll maxn = 1e5 + 2;
+
+vector<pair<int, int>> v[maxn];
+vector<int> dist(maxn, inf);
+priority_queue<pair<int, int>> q;
+bool processed[maxn];
 
 void build_dijkstra(int root){
     dist[root] = 0;
     q.push({0, root});
     while(!q.empty()){
-        int u = q.top().se;
+        int u = q.top().second;
         q.pop();
+        if (processed[u]) {
+            continue;
+        }
+        processed[u] = true;
         for(auto i:v[u]){
-            if(dist[i.se] > dist[u] + i.fi){
-                dist[i.se] = dist[u] + i.fi;
-                q.push({-dist[i.se], i.se});
+            if (processed[i.second]) {
+                continue;
+            }
+            if(dist[i.second] > dist[u] + i.first){
+                dist[i.second] = dist[u] + i.first;
+                q.push({-dist[i.second], i.second});
             }
         }
     }
